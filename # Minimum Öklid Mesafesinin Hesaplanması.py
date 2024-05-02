89c201279c71b95ded1faf3b2561a017f0708a8f
Hamza Çelik <147101453+HamzaCelikk@users.noreply.github.com>
2024-05-02 15:19:40 +0300

    Update # Minimum Öklid Mesafesinin Hesaplanması.py

--- # Minimum Öklid Mesafesinin Hesaplanması.py
+++ # Minimum Öklid Mesafesinin Hesaplanması.py
@@ -7,10 +7,10 @@ points = [(1, 2), (3, 4), (5, 6), (7, 8), (9, 10)]
 
 # Öklid Mesafesi için fonksiyon
 def euclideanDistance(point1, point2):
-#Öklid formülünü makinenin anlayacağı şekilde yazdık.
+#Öklid formülünü makinenin anlayacağı şekilde yazdık ( d = √(x₂-x₁)²+(y₂-y₁)² ).
   return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
 
-# Mesafelerin hesaplanması
+# Mesafelerin hesaplanması(Bir boş liste ve iç içe for döngüsü oluşturduk).
 distances = []
 for i in range(len(points)):
     for j in range(i+1, len(points)):
