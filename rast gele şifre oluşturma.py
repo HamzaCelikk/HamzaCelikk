c62bfcc802d61f0bf6da9c52723d16de67fa8e2d
Hamza Çelik <147101453+HamzaCelikk@users.noreply.github.com>
2024-10-29 13:20:25 +0300

    Update rast gele şifre oluşturma.py

--- rast gele şifre oluşturma.py
+++ rast gele şifre oluşturma.py
@@ -1,15 +1,11 @@
-#fonks ve listeleme
-
 import random
 import string
 
 def rastgele_sifre_olustur():
-    uzunluk =10
+    uzunluk =10  #şifre uzunluğu
     karakterler = string.ascii_letters + string.digits + string.punctuation
     sifre = ''.join(random.choice(karakterler) for _ in range(uzunluk))
     
-  
-    print(f"{sifre}: şifreniz:{sifre}.")
     return sifre
 
 print(rastgele_sifre_olustur())
